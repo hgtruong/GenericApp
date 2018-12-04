975698875e88c9788c03f29ec153df4e2aa554a6
fredricklou523 <fredrick.lou@gmail.com>
2018-12-03 21:30:48 -0800

    Boiler Plate for Generic App

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+//functional component AKA stateless component
+// const App = () => {
+// 	return <div>'I'M NOW RENDERED'</div>;
+// };
+
+//class component AKA stateful component
+class App extends React.Component {
+	render() {
+		return <div>'I AM NOW RENDERED'</div>;
+	}
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+//The Purpose of this file is to server as the entry point for our application
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
